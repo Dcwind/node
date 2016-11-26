09453c1c516afda85453bf71c4a5d22d350cac7a
DcWind <danielekwuazi@gmail.com>
2016-11-26 23:56:06 +0700

    server

--- server.js
+++ server.js
@@ -8,3 +8,30 @@ var server = http.createServer(function (request, response){
 
 
 server.listen(3000);
+
+var express = require('express');
+var app = express();
+
+var task = [
+  {
+    id: 1,
+    name: 'first phase project',
+  },
+  {
+    id: 2,
+    name: 'Do Economics homework'
+  }
+];
+
+var currentId = 2;
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.get('/tasks', function (req, res) {
+  res.send({tasks : task});
+});
+
+app.listen(PORT, function(){
+    console.log('Server listening on ' + PORT);
+});
