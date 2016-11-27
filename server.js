b86a49681aa1ae3f62ba51866138606b7404a04c
DcWind <danielekwuazi@gmail.com>
2016-11-27 19:50:07 +0700

    express added

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var http = require('http');
+// var http = require('http');
+//
+// var server = http.createServer(function (request, response){
+//   console.log("got a request!");
+//   response.write('Hiiiii'.toUpperCase());
+//   response.end();
+// });
 
-var server = http.createServer(function (request, response){
-  console.log("got a request!");
-  response.write('Hiiiii'.toUpperCase());
-  response.end();
-});
 
-
-server.listen(3000);
+// server.listen(3000);
 
 var express = require('express');
 var app = express();
