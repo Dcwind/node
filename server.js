9721a11a0ac6c34cb6c18b8d960b669a730c69f7
DcWind <danielekwuazi@gmail.com>
2016-11-28 22:56:35 +0700

    mini db

--- server.js
+++ server.js
@@ -12,6 +12,18 @@
 var express = require('express');
 var app = express();
 
+var task = [
+  {
+    id: 1,
+    name: 'first phase project',
+  },
+  {
+    id: 2,
+    name: 'Do Economics homework'
+  }
+];
+
+var currentId = 2;
 
 var PORT = process.env.PORT || 3000;
 
