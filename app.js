93ad56dbefad52fd9e91c0f76d89c872538694e2
DcWind <danielekwuazi@gmail.com>
2016-11-30 00:57:33 +0700

    view todolist

--- app.js
+++ app.js
@@ -10,12 +10,46 @@ $(function () {
           if(value){
             addItemTodo(value);
             $('#item').value = "";
-
           }
+
         }
 
       });
     });
+
+    // Add task function
+    function addItemTodo (text) {
+  var list = document.getElementById('todo');
+
+  var item = document.createElement('li');
+  item.innerText = text;
+
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+
+  var remove = document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML = removeSVG;
+
+  // Add click event for removing item
+  remove.addEventListener('click', removeItem);
+
+  var complete = document.createElement('button');
+  complete.classList.add('complete');
+  complete.innerHTML = completeSVG;
+
+  complete.addEventListener('click', completeItem);
+
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+
+  list.insertBefore(item, list.childNodes[0]);
+
+
+
+}
+
 
 });
 // using ajax
